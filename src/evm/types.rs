9270fa496674026f40b597c6ce387ecb776fd466
Platon Floria <platon.floria@gmail.com>
2023-08-13 18:59:37 +0800

    feat: token metadata query

---
+++ src/evm/types.rs
@@ -0,0 +1,85 @@
+use ethabi::token::Token;
+use web3::{
+    contract::tokens::Tokenizable,
+    types::{Address, U256},
+};
+
+
+#[derive(Clone, Debug)]
+pub struct Field {
+    pub x_min: f64,
+    pub y_min: f64,
+    pub x_max: f64,
+    pub y_max: f64,
+}
+
+impl Tokenizable for Field {
+    fn from_token(token: Token) -> Result<Self, web3::contract::Error> {
+        match token {
+            Token::Tuple(tokens) => {
+                Ok(Self { 
+                    x_min: U256::from_token(tokens[0].clone())?.as_u128() as f64 / 10_f64.powi(18) - 2.1,
+                    y_min: U256::from_token(tokens[1].clone())?.as_u128() as f64 / 10_f64.powi(18) - 1.5,
+                    x_max: U256::from_token(tokens[2].clone())?.as_u128() as f64 / 10_f64.powi(18) - 2.1,
+                    y_max: U256::from_token(tokens[3].clone())?.as_u128() as f64 / 10_f64.powi(18) - 1.5
+                })
+            }
+            _ => Err(web3::contract::Error::Abi(ethabi::Error::InvalidData)),
+        }
+    }
+
+    fn into_token(self) -> Token {
+        Token::Tuple(vec![
+            U256::from(((self.x_min + 2.1) * 10_f64.powi(18)) as u128).into_token(),
+            U256::from(((self.y_min + 1.5) * 10_f64.powi(18)) as u128).into_token(),
+            U256::from(((self.x_max + 2.1) * 10_f64.powi(18)) as u128).into_token(),
+            U256::from(((self.y_max + 1.5) * 10_f64.powi(18)) as u128).into_token(),
+        ])
+    }
+}
+
+impl web3::contract::tokens::TokenizableItem for Field {}
+
+
+#[derive(Clone, Debug)]
+pub struct Metadata {
+    pub token_id: u128,
+    pub owner: Address,
+    parent_id: u128,
+    pub field: Field,
+    pub locked_fuel: f64,
+    pub minimum_price: f64,
+    pub owned: bool,
+}
+
+impl Tokenizable for Metadata {
+    fn from_token(token: Token) -> Result<Self, web3::contract::Error> {
+        match token {
+            Token::Tuple(tokens) => {
+                Ok(Self { 
+                    token_id: U256::from_token(tokens[0].clone())?.as_u128(),
+                    owner: Address::from_token(tokens[1].clone())?,
+                    parent_id: U256::from_token(tokens[2].clone())?.as_u128(),
+                    field: Field::from_token(tokens[3].clone())?,
+                    locked_fuel: U256::from_token(tokens[4].clone())?.as_u128() as f64 / 10_f64.powi(18),
+                    minimum_price: U256::from_token(tokens[5].clone())?.as_u128() as f64 / 10_f64.powi(18),
+                    owned: false,
+                })
+            }
+            _ => Err(web3::contract::Error::Abi(ethabi::Error::InvalidData)),
+        }
+    }
+
+    fn into_token(self) -> Token {
+        Token::Tuple(vec![
+            self.token_id.into_token(),
+            self.owner.into_token(),
+            self.parent_id.into_token(),
+            self.field.into_token(),
+            U256::from(((self.locked_fuel) * 10_f64.powi(18)) as u128).into_token(),
+            U256::from(((self.minimum_price) * 10_f64.powi(18)) as u128).into_token(),
+        ])
+    }
+}
+
+impl web3::contract::tokens::TokenizableItem for Metadata {}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,107 @@
-use poem::{listener::TcpListener, Route, Server};
-use poem_openapi::{payload::PlainText, OpenApi, OpenApiService};
+mod evm;
 
-struct Api;
+use poem::{listener::TcpListener, Route, Server};
+use poem_openapi::{
+    param::Path,
+    payload::{Json, PlainText},
+    ApiResponse,
+    Object,
+    OpenApi,
+    OpenApiService,
+    Union
+};
+use web3::{
+    contract::{Contract, Options},
+    types::U256,
+    transports::Http,
+    Web3
+};
+
+
+#[derive(Union, Debug, PartialEq)]
+enum Value {
+    Int(u64),
+    String(String),
+    Float(f64),
+}
+
+
+#[derive(Debug, Object)]
+struct Attribute {
+    display_type: String,
+    trait_type: String,
+    value: Value,
+}
+
+
+#[derive(Debug, Object)]
+struct Metadata {
+    image: String,
+    external_url: String,
+    attributes: Vec<Attribute>,
+}
+
+
+#[derive(Debug, ApiResponse)]
+enum GetTokenResponse {
+    #[oai(status = 200)]
+    Ok(Json<Metadata>),
+    #[oai(status = 404)]
+    NotFound,
+}
+
+
+struct Api {
+    contract: Contract<Http>,
+}
 
 #[OpenApi]
 impl Api {
-    /// Hello world
-    #[oai(path = "/", method = "get")]
-    async fn index(&self) -> PlainText<&'static str> {
-        PlainText("Hello World")
+    fn new() -> Self {
+        let transport = Http::new(&std::env::var("NODE_RPC_URL").unwrap()).unwrap();
+        let web3 = Web3::new(transport);
+        Self {
+            contract: Contract::from_json(
+                web3.eth(),
+                std::env::var("ERC1155_CONTRACT_ADDRESS").unwrap().trim_start_matches("0x").parse().unwrap(),
+                include_bytes!("../resources/MandelbrotNFT.json"),
+            ).unwrap()
+        }
+    }
+
+    #[oai(path = "/:id", method = "get")]
+    async fn get(&self, id: Path<u64>) -> GetTokenResponse {
+        if let Ok(result) = self.contract.query(
+            "getMetadata",
+            (U256::from(*id),),
+            None,
+            Options::default(),
+            None
+        ).await {
+            let metadata: evm::types::Metadata = result;
+            GetTokenResponse::Ok(Json(Metadata {
+                image: String::new(),
+                external_url: format!("https://mandelbrot-nft.onrender.com/node/{}", *id),
+                attributes: vec![Attribute {
+                    display_type: "number".into(),
+                    trait_type: "Locked FUEL".into(),
+                    value: Value::Float(metadata.locked_fuel),
+                }],
+            }))
+        } else {
+            GetTokenResponse::NotFound
+        }
     }
 }
 
 
 #[tokio::main]
 async fn main() {
-    let api_service =
-        OpenApiService::new(Api, "Hello World", "1.0").server("https://mandelbrot-service.onrender.com");
+    dotenv::dotenv().ok();
+
+    let api_service = OpenApiService::new(Api::new(), "Hello World", "1.0")
+        .server("https://mandelbrot-service.onrender.com")
+        .server("http://127.0.0.1:10000");
     let ui = api_service.swagger_ui();
     let app = Route::new().nest("/", api_service).nest("/docs", ui);
     
